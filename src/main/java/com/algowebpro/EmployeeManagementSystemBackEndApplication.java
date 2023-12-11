115e4af4c2e69fe0098841483caf2eb022960235
swapnilk30 <sk9137587676@gmail.com>
2023-12-12 00:12:01 +0530

    initial project

---
+++ src/main/java/com/algowebpro/EmployeeManagementSystemBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.algowebpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algowebpro/EmployeeManagementSystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algowebpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
