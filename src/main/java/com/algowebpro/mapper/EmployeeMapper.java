5ff408118c68ebcc6452829fcfa98392ad2426d4
swapnilk30 <sk9137587676@gmail.com>
2023-12-12 00:49:33 +0530

    EmployeeMapper created..

---
+++ src/main/java/com/algowebpro/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.algowebpro.mapper;
+
+import com.algowebpro.dto.EmployeeDto;
+import com.algowebpro.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
